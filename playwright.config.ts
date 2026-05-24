5188f31049fa5169224f68936476bbd11862f898
thoufeeque <thoufeeque.rafique@gmail.com>
2026-05-24 10:50:17 +0200

    test: use 127.0.0.1 instead of localhost in playwright config to prevent timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:3005',
+    baseURL: 'http://127.0.0.1:3005',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     launchOptions: {
@@ -28,7 +28,7 @@ export default defineConfig({
   ],
   webServer: {
     command: 'npm run dev -- -p 3005',
-    url: 'http://localhost:3005',
+    url: 'http://127.0.0.1:3005',
     reuseExistingServer: true,
     stdout: 'pipe',
     stderr: 'pipe',
